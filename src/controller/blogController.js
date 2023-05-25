dcc79eb0406b8e0a0f8218ce535bd3e8748e6917
prajwal gaulkar <prajwalgaulkar78717@gmail.com>
2023-05-25 13:06:05 +0530

    post blog

--- src/controller/blogController.js
+++ src/controller/blogController.js
@@ -1,1 +1,62 @@
-const blogMode = require('../model/blogModel');
+const authorModel = require('../model/authorModel');
+const blogModel = require('../model/blogModel');
+
+const createBlog = async function (req, res) {
+    const blogDocument = req.body
+    const authorId = req.body.authorId
+
+    let validAuthor = await authorModel.findById(authorId)
+
+    try {
+        if (!validAuthor) {
+            return res.status(400).send({ status: false, msg: "" })
+        } else {
+            let postBlog = await blogModel.create(blogDocument)
+
+            return res.status(201).send({ status: true, data: postBlog })
+        }
+
+    } catch (err) {
+        res.status(500).send({ status: false, error: err })
+
+    }
+
+}
+
+module.exports.createBlog = createBlog
+
+
+
+// POST /blogs
+// Create a blog document from request body. Get authorId in request body only.
+
+// Make sure the authorId is a valid authorId by checking the author exist in the authors collection.
+
+// Return HTTP status 201 on a succesful blog creation. Also return the blog document. The response should be a JSON object like this
+
+// Create atleast 5 blogs for each author
+
+// Return HTTP status 400 for an invalid request with a response body like this
+
+
+
+
+
+
+
+
+
+
+
+
+
+// POST /blogs
+// Create a blog document from request body. Get authorId in request body only.
+
+// Make sure the authorId is a valid authorId by checking the author exist in the authors collection.
+
+// Return HTTP status 201 on a succesful blog creation. Also return the blog document. The response should be a JSON object like this
+
+// Create atleast 5 blogs for each author
+
+// Return HTTP status 400 for an invalid request with a response body like this
--- src/routes/route.js
+++ src/routes/route.js
@@ -1,5 +1,15 @@
 const express = require('express');
 const router = express.Router();
+const authorController= require('../controller/authorcontroller')
+const blogController= require('../controller/blogController')
+
+
+router.post('/blogs',blogController.createBlog)
 
 
 module.exports = router;
+
+
+
+
+
